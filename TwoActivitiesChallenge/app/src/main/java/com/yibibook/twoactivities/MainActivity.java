0a76380b4a9d906088aeaac6c913dc01a0f20bbd
yubinqiang@51talk.com <>
2019-12-17 17:19:39 +0800

    [U1 S2] TwoActivities Challenge Done

---
+++ TwoActivitiesChallenge/app/src/main/java/com/yibibook/twoactivities/MainActivity.java
@@ -0,0 +1,58 @@
+package com.yibibook.twoactivities;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "MESSAGE";
+    public static final int TEXT_REQUEST_CODE = 1;
+    private TextView mMessageTextView;
+    private TextView mHeaderReply;
+    private EditText mMessageEditText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mHeaderReply = findViewById(R.id.text_header_reply);
+        mMessageTextView = findViewById(R.id.textView_msg);
+        mMessageEditText = findViewById(R.id.editText_main);
+    }
+
+    public static final String LOG_TAG = MainActivity.class.getSimpleName();
+    public void launchSecondActivity(View view) {
+        if (! (view instanceof Button)) {
+            Log.d(LOG_TAG, "Button Clicked, but the argument view is not a instance of Button");
+            return;
+        }
+        Log.d(LOG_TAG, "Button Clicked!");
+        Button button = (Button )view;
+        Intent intent = new Intent(this, SecondActivity.class);
+        String msg = button.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, msg);
+        startActivityForResult(intent, TEXT_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode != RESULT_OK) {
+            return;
+        }
+
+        if (requestCode == TEXT_REQUEST_CODE) {
+            String msg = data.getStringExtra(SecondActivity.EXTRA_REPLY);
+            mHeaderReply.setVisibility(View.VISIBLE);
+            mMessageTextView.setVisibility(View.VISIBLE);
+            mMessageTextView.setText(msg);
+        }
+    }
+}
