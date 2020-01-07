6c0a396ba2f868086e8f63fc3b9a0cedf449717f
Milo <yubinqiang@51talk.com>
2020-01-07 11:00:45 +0800

    [U2 L3] Menu&Dialog

---
+++ DroidCafeInput/app/src/main/java/com/yibibook/droidcafeinput/DetailActivity.java
@@ -0,0 +1,41 @@
+package com.yibibook.droidcafeinput;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class DetailActivity extends AppCompatActivity {
+
+    private TextView detailTextView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        detailTextView = findViewById(R.id.detail);
+        registerForContextMenu(detailTextView);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        getMenuInflater().inflate(R.menu.menu_context_textview, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        String  msg = item.toString();
+        displayToast(msg);
+        return true;
+    }
+
+    private void displayToast(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+    }
+}
--- DroidCafeInput/app/src/main/java/com/yibibook/droidcafeinput/MainActivity.java
+++ DroidCafeInput/app/src/main/java/com/yibibook/droidcafeinput/MainActivity.java
@@ -6,6 +6,8 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
+import android.text.TextUtils;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -49,8 +51,30 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        String msg = "";
+        switch (id) {
+            case R.id.action_order:
+                msg = getString(R.string.action_order);
+                Intent intent = new Intent(this,
+                        DetailActivity.class);
+                intent.putExtra(EXTRA_MESSAGE, mOrderMessage);
+                startActivity(intent);
+                return true;
+            case R.id.action_status:
+                msg = getString(R.string.action_status);
+                break;
+            case R.id.action_favourite:
+                msg = getString(R.string.action_favourite);
+                break;
+            case R.id.action_contact:
+                msg = getString(R.string.action_contact);
+                break;
+            default:
+                break;
+        }
+
+        if (!TextUtils.isEmpty(msg)) {
+            displayToast(msg);
         }
 
         return super.onOptionsItemSelected(item);
